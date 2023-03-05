3ede3fdb5f47b7196d71ffe40f5fd0b56ad2a5c7
halang <lhr4884@naver.com>
2023-03-05 22:56:38 +0900

    chore: init (#1)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Noto_Sans_KR } from 'next/font/google'
+
+const NotoSansKR = Noto_Sans_KR({
+  weight: ['400', '700', '900'],
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${NotoSansKR.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
