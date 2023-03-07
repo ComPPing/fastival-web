f13f1e606892b40359966d8d7192182f18e5da13
leeharyung <lhr4884@naver.com>
2023-03-08 00:00:45 +0900

    feat: add google icon link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Noto_Sans_KR } from 'next/font/google'
+import { ThemeProvider } from '@emotion/react'
+
+import { theme } from '@/styles/theme'
+import '@/styles/globals.css'
 
 const NotoSansKR = Noto_Sans_KR({
   weight: ['400', '700', '900'],
@@ -10,12 +13,18 @@ const NotoSansKR = Noto_Sans_KR({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
+      />
       <style jsx global>{`
         html {
           font-family: ${NotoSansKR.style.fontFamily};
         }
       `}</style>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
