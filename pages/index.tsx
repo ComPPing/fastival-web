6b807ddf4a504d4b88eefd0ed2e5f85e433dfc06
halang <lhr4884@naver.com>
2023-03-08 14:06:46 +0900

    feat: add favicon (#4)
    * chore: add favicon
    
    * fix: change favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Fast!val</title>
         <meta name="description" content="유니들을 위한 축제 서비스" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon/favicon.ico" />
       </Head>
       <LandingPage />
     </>
