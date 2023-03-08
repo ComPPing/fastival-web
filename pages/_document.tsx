289a56966bfe8db755b11a6a09245c11bd7b8541
leeharyung <lhr4884@naver.com>
2023-03-09 01:18:08 +0900

    fix: relocating meta, link tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        {' '}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
+        />
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,6 @@ export default function Home() {
       <Head>
         <title>Fast!val</title>
         <meta name="description" content="유니들을 위한 축제 서비스" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon/favicon.ico" />
       </Head>
       <LandingPage />
     </>
