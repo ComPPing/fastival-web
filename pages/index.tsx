4f9add9d2ab1ce08d202905fe6ff4eb35ba28a8c
halang <lhr4884@naver.com>
2023-03-12 23:49:11 +0900

    feat: add og tag (#10)
    * fix: change favicon
    
    * rename: rename png
    
    * feat: add og tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,5 @@ import Head from 'next/head'
 import LandingPage from '@/components/template/landing-page'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Fast!val</title>
-        <meta name="description" content="유니들을 위한 축제 서비스" />
-      </Head>
-      <LandingPage />
-    </>
-  )
+  return <LandingPage />
 }
